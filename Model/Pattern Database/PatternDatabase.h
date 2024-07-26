3ddf356a0e02b114540701d0870a9205cb2cbb60
Akash Manna <112533557+AkashManna2003@users.noreply.github.com>
2024-07-27 02:19:10 +0530

    First Commit

---
+++ Model/Pattern Database/PatternDatabase.h
@@ -0,0 +1,19 @@
+/*
+ * Created by Akash on 01-07-2022.
+*/
+
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
